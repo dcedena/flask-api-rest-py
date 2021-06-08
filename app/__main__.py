7e26c5ce87224158dbcec9d9835a26a3284c6b99
David Cedena <david.cedena@gmail.com>
2021-06-08 21:44:41 +0200

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,4 @@
-from flask import Flask#, request, jsonify
+from flask import Flask
 from flask_restful import Api
 from models.Employees import Employees
 from models.EmployeeData import EmployeeData
